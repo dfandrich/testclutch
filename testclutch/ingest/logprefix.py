dfandrich/testclutch

commit 98ac2785f693af75a1f2588933dbdc3c93ef6bc4

Move reusable log file massagers into their own files
Generalize them a bit as well.

testclutch/ingest/logprefix.py
@@ -1,0 +1,49 @@
+"""Class to remove a prefix from each log line
+"""
+
+from typing import TextIO
+
+
+class FixedPrefixedLog(TextIO):
+    """Remove the timestamp at the head of every log line
+
+    Args:
+        prefixlen - length of prefix to remove
+    """
+    def __init__(self, f, prefixlen):
+        self.file_obj = f
+        self.prefixlen = prefixlen
+
+    def __getattr__(self, attr):
+        return getattr(self.file_obj, attr)
+
+    def readline(self):
+        l = self.file_obj.readline()
+        if l:
+            l = l[self.prefixlen:]
+        return l
+
+
+class RegexPrefixedLog(TextIO):
+    """TextIOWrapper that removes a matching regex at the head of every log line
+
+    Lines that don't match are sent through unchanged.
+    """
+    def __init__(self, f, regex):
+        self.file_obj = f
+        self.regex = regex
+
+    def __getattr__(self, attr):
+        return getattr(self.file_obj, attr)
+
+    def readline(self, size: int = -1):
+        l = self.file_obj.readline(size)
+        if l:
+            # Unfortunately, some log files have timestamps and some don't. It's probably something
+            # to do with embedded newlines in log messages, but I can't think of a more accurate way
+            # to remove them than with a regular expression. This will erroneously match "extended"
+            # log files that happen to include something that looks like a timestamp, but since
+            # these extended lines almost never happen in the first place (so far it seems only
+            # those using cross-platform-actions/action), this isn't a big concern.
+            l = self.regex.sub('', l)
+        return l

testclutch/ingest/msbuild.py
@@ -1,0 +1,53 @@
+"""Class to handle builds created by MSBuild
+"""
+
+from typing import TextIO
+
+
+class MsBuildLog(TextIO):
+    """Remove the indentation that msbuild adds to child output
+
+    This issue mentions the indentation that is done and implies that there is no way to
+    stop it (as of 2021, anyway):
+    https://github.com/dotnet/msbuild/issues/6614#issuecomment-866447382
+
+    This has been tested with msbuild ver. 4.8.3761.0, 15.9.21+g9802d43bc3 for .NET,
+    17.7.2+d6990bcfa for .NET
+    """
+    def __init__(self, f):
+        self.file_obj = f
+        self.in_msbuild = False
+
+    def __getattr__(self, attr):
+        return getattr(self.file_obj, attr)
+
+    def readline(self):
+        l = self.file_obj.readline()
+        if l.startswith('Microsoft (R) Build Engine') or l.startswith('MSBuild version '):
+            # Start of indented section
+            self.in_msbuild = True
+        elif self.in_msbuild:
+            # In indented section
+            if l.startswith('  '):
+                # Strip off indentation
+                l = l[2:]
+            elif l.startswith('CUSTOMBUILD : warning :'):
+                # This must be some kind of special msbuild escaping going on
+                l = 'Warning' + l[22:]
+
+            # Let through special cases: two strings that are part of the headers
+            # that begin the indented section, a completely empty line, and CUSTOMBUILD.
+            # Anything else not beginning with two spaces is the sign we've exited msbuild.
+            #
+            # NOTE: this is not a completely reliable indication. I don't know if CUSTOMBUILD
+            # is the only weird string to suddenly show up in the middle. This means that
+            # once we detect msbuild, we can't reliable switch out of it; there seems to be no
+            # magic string shown afterward, and there are cases where nonindented strings can
+            # appear in the middle. So, just leave it in dedenting mode once we detect msbuild
+            # is in use; it's highly likely that any log we're interested in in this case will
+            # be run under msbuild so it will work just fine.
+            #  elif (not l.startswith('[Microsoft .NET Framework')
+            #      and not l.startswith('Copyright (C) Microsoft Corporation')
+            #      and l.rstrip('\r\n')):
+            #      self.in_msbuild = False
+        return l

testclutch/ingest/appveyor.py
@@ -3,12 +3,14 @@
 import datetime
 import logging
 import re
-from typing import Any, Callable, Dict, Optional, TextIO
+from typing import Any, Callable, Dict, Optional
 
 from testclutch import db
 from testclutch import logcache
 from testclutch import summarize
 from testclutch.ingest import appveyorapi
+from testclutch.ingest import logprefix
+from testclutch.ingest import msbuild
 from testclutch.logdef import TestCases, TestMeta
 from testclutch.logparser import logparse
 
@@ -17,67 +19,6 @@ DEFAULT_EXT = '.log'
 LOGSUBDIR = 'appveyor'
 
 AV_TIME_RE = re.compile(r'^(\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d)(\.\d{1,7})([-+]\d\d):(\d\d)$')
-
-
-class MassagedLog(TextIO):
-    """Remove the timestamp at the head of every log line"""
-    def __init__(self, f):
-        self.file_obj = f
-
-    def __getattr__(self, attr):
-        return getattr(self.file_obj, attr)
-
-    def readline(self):
-        l = self.file_obj.readline()
-        if l:
-            l = l[11:]
-        return l
-
-
-class MsBuildLog(MassagedLog):
-    """Remove the indentation that msbuild adds to child output
-
-    This issue mentions the indentation that is done and implies that there is no way to
-    stop it (as of 2021, anyway):
-    https://github.com/dotnet/msbuild/issues/6614#issuecomment-866447382
-
-    This has been tested with msbuild ver. 4.8.3761.0, 15.9.21+g9802d43bc3 for .NET,
-    17.7.2+d6990bcfa for .NET
-    """
-    def __init__(self, f):
-        super().__init__(f)
-        self.in_msbuild = False
-
-    def readline(self):
-        l = super().readline()
-        if l.startswith('Microsoft (R) Build Engine') or l.startswith('MSBuild version '):
-            # Start of indented section
-            self.in_msbuild = True
-        elif self.in_msbuild:
-            # In indented section
-            if l.startswith('  '):
-                # Strip off indentation
-                l = l[2:]
-            elif l.startswith('CUSTOMBUILD : warning :'):
-                # This must be some kind of special msbuild escaping going on
-                l = 'Warning' + l[22:]
-
-            # Let through special cases: two strings that are part of the headers
-            # that begin the indented section, a completely empty line, and CUSTOMBUILD.
-            # Anything else not beginning with two spaces is the sign we've exited msbuild.
-            #
-            # NOTE: this is not a completely reliable indication. I don't know if CUSTOMBUILD
-            # is the only weird string to suddenly show up in the middle. This means that
-            # once we detect msbuild, we can't reliable switch out of it; there seems to be no
-            # magic string shown afterward, and there are cases where nonindented strings can
-            # appear in the middle. So, just leave it in dedenting mode once we detect msbuild
-            # is in use; it's highly likely that any log we're interested in in this case will
-            # be run under msbuild so it will work just fine.
-            #  elif (not l.startswith('[Microsoft .NET Framework')
-            #      and not l.startswith('Copyright (C) Microsoft Corporation')
-            #      and l.rstrip('\r\n')):
-            #      self.in_msbuild = False
-        return l
 
 
 class AppveyorIngestor:
@@ -220,7 +161,8 @@ class AppveyorIngestor:
                          log_processor: Callable[[TestMeta, TestCases], None]):
         logging.debug('Processing file %s', fn)
         # TODO: Assuming local charset; probably convert from ISO-8859-1 instead
-        readylog = MsBuildLog(logcache.open_cache_file(fn))
+        readylog = msbuild.MsBuildLog(
+            logprefix.FixedPrefixedLog(logcache.open_cache_file(fn), prefixlen=11))
         meta, testcases = logparse.parse_log_file(readylog)
         if meta:
             # combine ci metadata with metadata from log file

testclutch/ingest/azure.py
@@ -3,12 +3,13 @@
 import datetime
 import logging
 import re
-from typing import Any, Dict, Iterable, Optional, TextIO
+from typing import Any, Dict, Iterable, Optional
 
 from testclutch import db
 from testclutch import logcache
 from testclutch import summarize
 from testclutch.ingest import azureapi
+from testclutch.ingest import logprefix
 from testclutch.logdef import TestCases, TestMeta
 from testclutch.logparser import logparse
 
@@ -23,21 +24,6 @@ AV_TIME_RE = re.compile(r'^(\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d)(\.\d{1,7})?Z$')
 # Tasks created by Azure, whose logs we don't care about
 SYSTEM_TASKS_RE = re.compile(r'^Initialize job$|^Initialize containers$|^Stop Containers$|'
                              r'^Finalize Job$|^Checkout |^Post-job: ')
-
-
-class MassagedLog(TextIO):
-    """Remove the timestamp at the head of every log line"""
-    def __init__(self, f):
-        self.file_obj = f
-
-    def __getattr__(self, attr):
-        return getattr(self.file_obj, attr)
-
-    def readline(self):
-        l = self.file_obj.readline()
-        if l:
-            l = l[29:]
-        return l
 
 
 class AzureIngestor:
@@ -252,7 +238,7 @@ class AzureIngestor:
     def ingest_log_file(self, fn: str, cimeta: TestMeta):
         logging.debug('Ingesting file %s', fn)
         # TODO: Assuming local charset; probably convert from ISO-8859-1 instead
-        readylog = MassagedLog(logcache.open_cache_file(fn))
+        readylog = logprefix.FixedPrefixedLog(logcache.open_cache_file(fn), prefixlen=29)
         meta, testcases = logparse.parse_log_file(readylog)
         if meta:
             # combine ci metadata with metadata from log file

testclutch/ingest/gha.py
@@ -15,6 +15,7 @@ from testclutch import db
 from testclutch import logcache
 from testclutch import summarize
 from testclutch.ingest import ghaapi
+from testclutch.ingest import logprefix
 from testclutch.logdef import TestCases, TestMeta
 from testclutch.logparser import logparse
 
@@ -55,21 +56,6 @@ def read_token(authfile: Optional[str]) -> Optional[str]:
         return None
     with open(authfile) as tokfile:
         return tokfile.read().strip()
-
-
-class MassagedLog(io.TextIOWrapper):
-    """TextIOWrapper that removes the timestamp at the head of every log line"""
-    def readline(self, size: int = -1):
-        l = super().readline(size)
-        if l:
-            # Unfortunately, some log files have timestamps and some don't. It's probably something
-            # to do with embedded newlines in log messages, but I can't think of a more accurate way
-            # to remove them than with a regular expression. This will erroneously match "extended"
-            # log files that happen to include something that looks like a timestamp, but since
-            # these extended lines almost never happen in the first place (so far it seems only
-            # those using cross-platform-actions/action), this isn't a big concern.
-            l = LOG_TIMESTAMP_RE.sub('', l)
-        return l
 
 
 class GithubIngestor:
@@ -263,7 +249,9 @@ class GithubIngestor:
                 logging.debug('Skipping %s', fileinfo.filename)
                 continue
             logging.debug('Processing member %s', fileinfo.filename)
-            readylog = MassagedLog(log.open(fileinfo.filename), encoding=LOG_CHARMAP)
+            readylog = logprefix.RegexPrefixedLog(
+                io.TextIOWrapper(log.open(fileinfo.filename), encoding=LOG_CHARMAP),
+                regex=LOG_TIMESTAMP_RE)
             meta, testcases = logparse.parse_log_file(readylog)
             if meta:
                 # combine ci metadata with metadata from log file

commit 9b6623ac78ed43fec06b4d59af97e30382715e60

Fix pytype log parsing on GHA
The log filter classes were switched to derive directly from TextIO in
commit 98ac2785 which caused the seek method to go to the empty one in
the base class instead of the real one on the embedded file member.
Since the TextIO was there only for typing purposes, eliminate it and
use a (very ugly) synthetic type class instead. Unfortunately, I don't
know of a cleaner way to keep typing on these classes that satisfies
pytype. Even this doesn't even work with pytype on Python 3.11, so
disable that version in the CI.

Fix MsBuildLog to reset its in_msbuild flag when the file is rewound.

Fix the filters' readline() to all support the size argument.

.github/workflows/python.yml
@@ -40,8 +40,9 @@ jobs:
           flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
           # stop the build on any other warnings, too
           flake8 . --count --statistics
-      - if: matrix.python-version != '3.12'
+      - if: matrix.python-version != '3.11' && matrix.python-version != '3.12'
         # pytype as of 2024.4.11 doesn't support Python 3.12
+        # pytype 2024.4.11 has a bug recognizing FakeDerivedTextIOWithArgs only on Python 3.11
         name: Type check with pytype
         run: |
           pytype -j auto $(find testclutch tests -name '*.py')

testclutch/ingest/logprefix.py
@@ -1,39 +1,59 @@
 """Class to remove a prefix from each log line
 """
 
+import re
 from typing import TextIO
 
 
-class FixedPrefixedLog(TextIO):
+class FakeDerivedTextIOWithArgs(TextIO):
+    """This class is only used for typing hints for duck typing TextIO, no in code
+
+    We don't want to actually derive from TextIO in the classes that need this because then calls to
+    the object's members won't be caught by the __getattr__ function and the wrong member will be
+    accessed.
+    """
+    def __init__(self, *a1, **args):
+        pass
+
+    def __call__(self, *a2, **args) -> 'FakeDerivedTextIOWithArgs':
+        "For some reason, pytype doesn't see that constructing this object actually uses __init__"
+        return self
+
+
+# pytype: disable=annotation-type-mismatch
+class FixedPrefixedLog:  # type: FakeDerivedTextIOWithArgs
+    # pytype: enable=annotation-type-mismatch
     """Remove the timestamp at the head of every log line
 
     Args:
         prefixlen - length of prefix to remove
     """
-    def __init__(self, f, prefixlen):
+    def __init__(self, f: TextIO, prefixlen: int):
         self.file_obj = f
         self.prefixlen = prefixlen
 
-    def __getattr__(self, attr):
+    def __getattr__(self, attr: str):
         return getattr(self.file_obj, attr)
 
-    def readline(self):
-        l = self.file_obj.readline()
+    def readline(self, size: int = -1):
+        l = self.file_obj.readline(size)
         if l:
             l = l[self.prefixlen:]
         return l
 
 
-class RegexPrefixedLog(TextIO):
+# pytype: disable=annotation-type-mismatch
+class RegexPrefixedLog:  # type: FakeDerivedTextIOWithArgs
+    # pytype: enable=annotation-type-mismatch
     """TextIOWrapper that removes a matching regex at the head of every log line
 
     Lines that don't match are sent through unchanged.
     """
-    def __init__(self, f, regex):
+    def __init__(self, f: TextIO, regex: re.Pattern):
         self.file_obj = f
         self.regex = regex
 
-    def __getattr__(self, attr):
+    def __getattr__(self, attr: str):
         return getattr(self.file_obj, attr)
 
     def readline(self, size: int = -1):

testclutch/ingest/msbuild.py
@@ -3,8 +3,12 @@
 
 from typing import TextIO
 
+from testclutch.ingest.logprefix import FakeDerivedTextIOWithArgs  # noqa: F401
 
-class MsBuildLog(TextIO):
+
+# pytype: disable=annotation-type-mismatch
+class MsBuildLog:  # type: FakeDerivedTextIOWithArgs
+    # pytype: enable=annotation-type-mismatch
     """Remove the indentation that msbuild adds to child output
 
     This issue mentions the indentation that is done and implies that there is no way to
@@ -14,15 +18,23 @@ class MsBuildLog(TextIO):
     This has been tested with msbuild ver. 4.8.3761.0, 15.9.21+g9802d43bc3 for .NET,
     17.7.2+d6990bcfa for .NET
     """
-    def __init__(self, f):
+    def __init__(self, f: TextIO):
         self.file_obj = f
         self.in_msbuild = False
 
-    def __getattr__(self, attr):
+    def __getattr__(self, attr: str):
+        "Send everything else to the embedded file"
         return getattr(self.file_obj, attr)
 
-    def readline(self):
-        l = self.file_obj.readline()
+    def seek(self, offset: int, whence: int = 0):
+        "Capture to seek to reset the state"
+        if offset == 0 and whence < 16:
+            # Stream is starting again from (near) the beginning
+            self.in_msbuild = False
+        return self.file_obj.seek(offset, whence)
+
+    def readline(self, size: int = -1):
+        l = self.file_obj.readline(size)
         if l.startswith('Microsoft (R) Build Engine') or l.startswith('MSBuild version '):
             # Start of indented section
             self.in_msbuild = True