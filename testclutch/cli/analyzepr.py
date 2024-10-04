dfandrich/testclutch

commit a7d4a91e2e1c7e6e748bfce167f9cd07141cb0d0

Minor simplifications

testclutch/cli/analyzepr.py
@@ -277,8 +277,7 @@ def analyze_pr_html(repo: str, pr: int, test_results: Sequence[ParsedLog], ds: d
         print('</td>')
 
         # Finally, show results of PR
-        jobtime = (meta['runtriggertime'] if 'runtriggertime' in meta else
-                   meta['runstarttime'] if 'runstarttime' in meta else meta['runfinishtime'])
+        jobtime = meta.get('runtriggertime', meta.get('runstarttime', meta['runfinishtime']))
         # title must contain safe HTML as it will not be escaped
         # TODO: include commit ID in message
         title = datetime.datetime.fromtimestamp(

testclutch/db.py
@@ -123,8 +123,7 @@ class Datastore:
         self.db.commit()
 
     def store_test_run(self, meta: TestMeta, testresults: TestCases):
-        index_time = (meta['runtriggertime'] if 'runtriggertime' in meta else
-                      meta['runstarttime'] if 'runstarttime' in meta else meta['runfinishtime'])
+        index_time = meta.get('runtriggertime', meta.get('runstarttime', meta['runfinishtime']))
         repo = meta['checkrepo']
         origin = meta['origin']
         # Sqlite3 doesn't enforce UNIQUE if this is None (a.k.a. NULL), only ''

testclutch/logparser/curlparse.py
@@ -528,7 +528,7 @@ def parse_log_file(f: TextIOReadline) -> ParsedLog:  # noqa: C901
             # source of this log. It is identical to the buildcode used internally on the
             # page https://curl.se/dev/builds.html
             if r.group(1) not in TESTCURLBUILDCODEIGNORED:
-                current_code = meta['buildcode'] if 'buildcode' in meta else 0
+                current_code = meta.get('buildcode', 0)
                 # Use ISO 8859/1 to avoid any kind of encoding error; it hashes just as well
                 # as anything else
                 meta['buildcode'] = zlib.crc32(l.strip().encode('ISO-8859-1'), current_code)

commit 133673112955400e4b4df810ab9340864b323f33

Avoid indexing on runfinishtime in case it's not there
Use get when finding the job time from a dict to avoid a KeyError.
This fixes a problem introduced in a7d4a91e.

testclutch/cli/analyzepr.py
@@ -277,7 +277,7 @@ def analyze_pr_html(repo: str, pr: int, test_results: Sequence[ParsedLog], ds: d
         print('</td>')
 
         # Finally, show results of PR
-        jobtime = meta.get('runtriggertime', meta.get('runstarttime', meta['runfinishtime']))
+        jobtime = meta.get('runtriggertime', meta.get('runstarttime', meta.get('runfinishtime')))
         # title must contain safe HTML as it will not be escaped
         # TODO: include commit ID in message
         title = datetime.datetime.fromtimestamp(

testclutch/db.py
@@ -123,7 +123,7 @@ class Datastore:
         self.db.commit()
 
     def store_test_run(self, meta: TestMeta, testresults: TestCases):
-        index_time = meta.get('runtriggertime', meta.get('runstarttime', meta['runfinishtime']))
+        index_time = meta.get('runtriggertime', meta.get('runstarttime', meta.get('runfinishtime')))
         repo = meta['checkrepo']
         origin = meta['origin']
         # Sqlite3 doesn't enforce UNIQUE if this is None (a.k.a. NULL), only ''