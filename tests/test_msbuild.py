0256eabfa7112a5fa7fb61382e2ad9e63ecd0c00
Dan Fandrich <dan@telarity.com>
2025-04-14 18:02:28 -0700

    Test the MsBuildLog seek implementation
    This would have caught the problem fixed by 0384a1a3.

--- tests/test_msbuild.py
+++ tests/test_msbuild.py
@@ -40,3 +40,36 @@ class TestMsBuildLog(unittest.TestCase):
             'Final unindented line\n'
         ],
             lines)
+
+    def test_msbuildlog_seek(self):
+        infile = io.StringIO(textwrap.dedent("""\
+            First line
+              Indented Second line
+            Microsoft (R) Build Engine version 4.8.3761.0
+              Indented build line
+              Another indented
+                Even more indented, and staying there
+        """))
+        msbuildlog = msbuild.MsBuildLog(infile)
+        lines = list(iter(msbuildlog.readline, ''))
+        self.assertEqual([
+            'First line\n',
+            '  Indented Second line\n',
+            'Microsoft (R) Build Engine version 4.8.3761.0\n',
+            'Indented build line\n',
+            'Another indented\n',
+            '  Even more indented, and staying there\n',
+        ],
+            lines)
+
+        # Seek past the first line
+        msbuildlog.seek(11, 0)
+        lines = list(iter(msbuildlog.readline, ''))
+        self.assertEqual([
+            '  Indented Second line\n',
+            'Microsoft (R) Build Engine version 4.8.3761.0\n',
+            'Indented build line\n',
+            'Another indented\n',
+            '  Even more indented, and staying there\n',
+        ],
+            lines)
