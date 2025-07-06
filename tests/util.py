28a99a93fdba5d117c88064dd4d5840600cfdc5d
Dan Fandrich <dan@telarity.com>
2025-07-05 17:15:04 -0700

    Improve patch_config_get to default to the original config option
    This allows overriding one item but return the configured values for all
    the others. Multiple items can be overridden by calling the function
    more than once.

--- tests/util.py
+++ tests/util.py
@@ -4,6 +4,8 @@ import os
 from typing import TextIO
 from unittest.mock import patch
 
+from testclutch import config
+
 
 # Directory holding test data files
 DATADIR = 'data'
@@ -22,7 +24,14 @@ def open_data(fn: str) -> TextIO:
 def patch_config_get(key: str, value):
     """Mock config.get() to return a specific value for a given key.
 
-    All other values return None, so this should only be used when a single config.get() is
-    expected.
+    All other keys return the originally-configured value. Multiple items can be overridden by
+    calling this more than once, but it cannot be used as a decorator in that case; it must
+    be called within the test (for example as a context manager) because each patch must have access
+    to the mock installed by the previous call, which isn't the case when called as a decorator.
     """
-    return patch('testclutch.config.get', side_effect=lambda k: value if k == key else None)
+    def side_effect(k: str):
+        return value if k == key else orig_get(k)
+
+    # Use the original (or the previously-patched) get() for unmatched keys
+    orig_get = config.get
+    return patch('testclutch.config.get', side_effect=side_effect)
