4ea721a57cfae199950a651baaa0b0a8216e8d3e
Viktor Szakats <commit@vsz.me>
2026-03-20 00:04:22 +0100

    pyplatform: determine arch from 'mach' on macOS
    'mach' returns `arm64` on arm machines, and `x86_64` on Intel. To align
    pytest host arch in the test matrix with the other entries (and with
    that the arch really is.)
    
    Before this patch these values were `arm` and `i386`.
    
    Example values returned by Python `platform.platform()`:
    macOS-15.7.3-arm64-arm-64bit-Mach-O
    macOS-12.7.6-x86_64-i386-64bit

--- testclutch/pyplatform.py
+++ testclutch/pyplatform.py
@@ -48,7 +48,7 @@ def parse_platform(platform: str) -> TestMetaStr:
     elif (platparts[0] == 'macOS' and (r := PLAT_MACOS_RE.search(platform))
           or (r := PLAT_DEFAULT_RE.search(platform))):
         meta['systemosver'] = r.group('release')
-        meta['arch'] = r.group('proc')
+        meta['arch'] = r.group('mach')
         meta['archbits'] = r.group('bits')
 
     return meta
