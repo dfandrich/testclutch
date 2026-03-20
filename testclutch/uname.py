7e83d731aa1de0809d535637c11a2769fa786aee
Viktor Szakats <vszakats@users.noreply.github.com>
2026-03-21 00:43:00 +0100

    logparser: prefer `RE_BI_HOSTCPU` for MSYS2/Cygwin shells (#3)
    * logparser: prefer `RE_BI_HOSTCPU` for MSYS2/Cygwin shells
    
    If present, it accurately reports the architecture of the operating
    system, whereas the `uname -s` value used prior to this patch reports
    the architecture of the MSYS2/Cygwin shell binaries. This is always
    x86_64 on modern systems, even if the machine is a native ARM64 one.
    
    Ref: https://github.com/dfandrich/testclutch/pull/2#issuecomment-4094263147
    
    Co-authored-by: Dan Fandrich <dan@telarity.com>

--- testclutch/uname.py
+++ testclutch/uname.py
@@ -60,12 +60,14 @@ def parse_uname(uname: str) -> TestMetaStr:
           or meta['systemos'].startswith('MINGW32_NT')
           or meta['systemos'].startswith('MINGW64_NT')
           or meta['systemos'].startswith('CYGWIN_NT')) and len(sysparts) == 8:
+        # This reflects the architecture of the shell binaries, not the operating system's.
         meta['arch'] = sysparts[-2]
     elif (meta['systemos'].startswith('MSYS_NT')
           or meta['systemos'].startswith('MINGW32_NT')
           or meta['systemos'].startswith('MINGW64_NT')
           or meta['systemos'].startswith('CYGWIN_NT')) and len(sysparts) == 7:
         # This version is missing the time zone
+        # This reflects the architecture of the shell binaries, not the operating system's.
         meta['arch'] = sysparts[-2]
     elif meta['systemos'] == 'Windows_NT' and len(syspartsblanks) == 6:
         # systemosver as set above is just the major release number
