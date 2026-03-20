566b0e58e953c2d640e78ac37bf3b9fb83488ab3
Viktor Szakats <commit@vsz.me>
2026-03-20 20:37:57 +0100

    indent on the blind

--- testclutch/logparser/curlparse.py
+++ testclutch/logparser/curlparse.py
@@ -264,9 +264,9 @@ def parse_buildinfo(l: str) -> TestMetaStr:
         # architecture of the shell binaries, not the operating system's.
         if ('systemos' in meta
             and (meta['systemos'].startswith('MSYS_NT')
-                or meta['systemos'].startswith('MINGW32_NT')
-                or meta['systemos'].startswith('MINGW64_NT')
-                or meta['systemos'].startswith('CYGWIN_NT'))):
+                 or meta['systemos'].startswith('MINGW32_NT')
+                 or meta['systemos'].startswith('MINGW64_NT')
+                 or meta['systemos'].startswith('CYGWIN_NT'))):
             meta['arch'] = meta['hostarch']
     elif r := RE_BI_HOSTOS.search(l):
         meta['hostos'] = r.group(1)
